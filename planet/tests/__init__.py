6f4f83b9a769efcbf0a1841355d6a19d270dde8d
Martino Pizzol <martino@ahref.eu>
2012-02-08 17:46:35 +0100

    DRY :)

--- planet/tests/__init__.py
+++ planet/tests/__init__.py
@@ -7,7 +7,6 @@ from django.test import TestCase
 
 from planet.models import Post, Feed
 
-
 class DjangoPlanet(TestCase):
     """
     DjangoPlanet tests
@@ -27,29 +26,20 @@ class DjangoPlanet(TestCase):
         """
         import feedparser
         original_parse = feedparser.parse
-        def monkey_parse_old(url_file_stream_or_string, etag=None, modified=None,
+        rss_name = self.iwatch_old
+
+        def monkey_parse(url_file_stream_or_string, etag=None, modified=None,
                              agent=None, referrer=None, handlers=None,
                              request_headers=None, response_headers=None):
             file = open(os.path.dirname(os.path.abspath(__file__)) + '/' +
-                        self.iwatch_old, 'r')
-            rss_content = file.read()
-            file.close()
-            return original_parse(rss_content, etag, modified, agent, referrer,
-                                  handlers, request_headers, response_headers)
-        monkey_parse_old.__name__ = 'parse'
-
-        def monkey_parse(url_file_stream_or_string, etag=None, modified=None,
-                         agent=None, referrer=None, handlers=None,
-                         request_headers=None, response_headers=None):
-            file = open(os.path.dirname(os.path.abspath(__file__)) + '/' +
-                        self.iwatch, 'r')
+                        rss_name, 'r')
             rss_content = file.read()
             file.close()
             return original_parse(rss_content, etag, modified, agent, referrer,
                                   handlers, request_headers, response_headers)
         monkey_parse.__name__ = 'parse'
 
-        setattr(feedparser, feedparser.parse.__name__, monkey_parse_old )
+        setattr(feedparser, feedparser.parse.__name__, monkey_parse)
 
         call_command('add_feed', 'http://www.iwatachnews.or/rss/')
         post_count = Post.objects.count()
@@ -57,8 +47,8 @@ class DjangoPlanet(TestCase):
 
         self.assertEqual(post_count, 16)
         self.assertEqual(feed_count, 1)
-
-        setattr(feedparser, feedparser.parse.__name__, monkey_parse )
-        call_command('update_all_feeds')
+        rss_name = self.iwatch
+        setattr(feedparser, feedparser.parse.__name__, monkey_parse)
+        call_command('update_all_feeds_parallel')
         post_count = Post.objects.count()
         self.assertEqual(post_count, 18)
