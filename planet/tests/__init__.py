3358351882e875fd775446eb3195f72e607dfaba
Martino Pizzol <martino@ahref.eu>
2012-02-09 09:37:39 +0100

    Refactoring test

--- planet/tests/__init__.py
+++ planet/tests/__init__.py
@@ -7,27 +7,22 @@ from django.test import TestCase
 
 from planet.models import Post, Feed
 
-class DjangoPlanet(TestCase):
-    """
-    DjangoPlanet tests
-    """
-    def setUp(self):
-        """
-        setUp function
-        """
-        self.iwatch = 'iwatchnews.rss'
-        self.iwatch_old = 'iwatchnews_old.rss'
-        self.will = 'willmcguagan.rss'
-        self.will_old = 'willmcguagan_old.rss'
+import feedparser
+original_parse = feedparser.parse
 
-    def test_update_feed(self):
+class DjangoPlanetBaseTest(TestCase):
+    """
+    DjangoPlanetBaseTest tests
+    """
+    iwatch = 'iwatchnews.rss'
+    iwatch_old = 'iwatchnews_old.rss'
+    will = 'willmcguagan.rss'
+    will_old = 'willmcguagan_old.rss'
+
+    def update_feed(self, rss_name):
         """
         test add and update feed
         """
-        import feedparser
-        original_parse = feedparser.parse
-        rss_name = self.iwatch_old
-
         def monkey_parse(url_file_stream_or_string, etag=None, modified=None,
                              agent=None, referrer=None, handlers=None,
                              request_headers=None, response_headers=None):
@@ -38,17 +33,30 @@ class DjangoPlanet(TestCase):
             return original_parse(rss_content, etag, modified, agent, referrer,
                                   handlers, request_headers, response_headers)
         monkey_parse.__name__ = 'parse'
-
         setattr(feedparser, feedparser.parse.__name__, monkey_parse)
 
+
+class TestCommands(DjangoPlanetBaseTest):
+    def test_update_all_feed(self):
+        self.update_feed(self.iwatch_old)
         call_command('add_feed', 'http://www.iwatachnews.or/rss/')
         post_count = Post.objects.count()
         feed_count = Feed.objects.count()
 
         self.assertEqual(post_count, 16)
         self.assertEqual(feed_count, 1)
-        rss_name = self.iwatch
-        setattr(feedparser, feedparser.parse.__name__, monkey_parse)
-        call_command('update_all_feeds_parallel')
+
+        self.update_feed(self.iwatch)
+        call_command('update_all_feeds')
         post_count = Post.objects.count()
         self.assertEqual(post_count, 18)
+
+    def test_add_feed(self):
+        self.update_feed(self.iwatch_old)
+        call_command('add_feed', 'http://www.iwatachnews.or/rss/')
+        post_count = Post.objects.count()
+        feed_count = Feed.objects.count()
+
+        self.assertEqual(post_count, 16)
+        self.assertEqual(feed_count, 1)
+
