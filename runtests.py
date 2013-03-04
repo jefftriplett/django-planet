bc0875951f6bbf7972bf749f20ccaaf2aad3e028
Jeff Triplett <jeff.triplett@gmail.com>
2013-03-04 17:03:00 -0600

    Updated runtests.py, refactoring tests, and updated coverage.

--- runtests.py
+++ runtests.py
@@ -1,48 +1,35 @@
 #!/usr/bin/env python
-
-"""
-runtests
-~~~~~~~~
-
-:copyright: (c) 2010 DISQUS.
-:license: Apache License 2.0, see LICENSE for more details.
-"""
-
 import sys
-from os.path import dirname, abspath
 
-from django.core.management import call_command
 from django.conf import settings
 
-import test_settings
 
+settings.configure(
+    DATABASES={
+        'default': {'ENGINE': 'django.db.backends.sqlite3', 'NAME': ':memory;'}
+    },
+    INSTALLED_APPS=[
+        'django.contrib.auth',
+        'django.contrib.sites',
+        'django.contrib.sessions',
+        'django.contrib.contenttypes',
+        'tagging',
+        'planet',
+    ],
+    ROOT_URLCONF='planet.urls',
+    SITE_ID=1,
+    TEST_RUNNER='django_coverage.coverage_runner.CoverageRunner',
+    USER_AGENT='TestBrowser',
+)
 
-if not settings.configured:
-    settings.configure(
-        **vars(test_settings)
-    )
 
 def runtests(*test_args):
-    from django.test.simple import run_tests
+    import django.test.utils
+    runner_class = django.test.utils.get_runner(settings)
+    test_runner = runner_class(verbosity=1, interactive=True)
+    failures = test_runner.run_tests(['planet'])
+    sys.exit(failures)
 
-    if 'south' in settings.INSTALLED_APPS:
-        from south.management.commands import patch_for_test_db_setup
-        patch_for_test_db_setup()
-
-    if not test_args:  ## setup.py test
-        test_args = ['planet', '--jenkins']
-    parent = dirname(abspath(__file__))
-    sys.path.insert(0, parent)
-    if '--jenkins' in test_args:
-        call_command('jenkins', 'planet', verbosity=1, interactive=True)
-        sys.exit(0)
-    else:
-        failures = run_tests(test_args)
-        sys.exit(failures)
 
 if __name__ == '__main__':
-    argv = sys.argv[1:]
-    if not argv:
-        argv = ['planet']
-
-    runtests(*argv)
+    runtests()
